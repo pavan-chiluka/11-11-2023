8e512620baa1bf48cd8a6167ce39b425add79549
pavan <pavan.chiluka@thundersoft.com>
2023-10-11 18:26:11 +0530

    stringprograms

---
+++ removeduplicate.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s1[50];
+	scanf("%s",s1);
+        int i,j,k;
+	for(i=0;s1[i]!='\0';i++)
+	{
+		for(j=i+1;s1[j]!='\0';j++)
+		{
+			if(s1[i]==s1[j])
+			{
+
+			 for(k=j;s1[k]!='\0';k++)
+			 {
+				 s1[k]=s1[k+1];
+			 }
+			}
+		}
+	}
+	printf("%s",s1);
+}
